60acac1d2d30c7ff11a3009ba899be41b2aaf3c6
Andres <andres@MacBook-Air-Andres.local>
2026-03-24 07:26:32 +0000

    version v30 + мультиволатильность

--- frontend/src/components/Layout/TopNav.jsx
+++ frontend/src/components/Layout/TopNav.jsx
@@ -54,7 +54,7 @@ function TopNav() {
           <span className="text-sm text-cyan-500 font-medium">v33</span>
         )}
         {location.pathname === '/tools/universal-calculator' && (
-          <span className="text-sm text-cyan-500 font-medium">v29 + коррекция волатильности</span>
+          <span className="text-sm text-cyan-500 font-medium">v30 + мультиволатильность</span>
         )}
       </div>
 
